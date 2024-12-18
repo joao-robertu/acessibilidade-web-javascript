0cb54bc21b83e1e232c751ddb5e1820c397b5dc7
João Roberto <joao.adsbrasil@gmail.com>
2024-12-17 22:53:58 -0300

    Fim do curso

--- 3489-calmaria-spa-javascript-aula/main.js
+++ 3489-calmaria-spa-javascript-aula/main.js
@@ -1,10 +1,50 @@
+let ultimoElementoFocado;
+
+function gerenciarFocoModal(modalId) {
+  const modal = document.querySelector(`#${modalId}`);
+  const elementosModal = modal.querySelectorAll('a, button, input, select, textarea, [tabindex]:not([tabindex:="-1])');  
+
+  const primeiroElemento = elementosModal[0]
+  const ultimoElemento = elementosModal[elementosModal.length - 1];
+
+  primeiroElemento.focus();
+
+  modal.addEventListener('keydown', (event) => {
+    if(event.key === "tab") {
+      if(event.shiftKey) {
+        // se a tecla Shift+Tab for pressionada , e o foco estiver no primeiro elemento, mover para o ultimo
+        if(document.activeElement === primeiroElemento) {
+          event.preventDefault();
+          ultimoElemento.focus();
+        }
+      } else {
+        // se a tecla Tab for pressionada, e o foco estiver no ultimo, mover para o primeiro
+        if (
+          document.activeElement === ultimoElemento ||
+          !modal.contains(document.activeElement)
+      ) {
+          event.preventDefault();
+          primeiroElemento.focus();
+        }
+      }
+    }
+  });
+}
+
 function alternarModal(modalId, abrir) {
   const modal = document.querySelector(`#${modalId}`);
 
   if (abrir) {
+    ultimoElementoFocado = document.activeElement;
+
     modal.style.display = "block";
+    gerenciarFocoModal(modalId);
   } else {
     modal.style.display = "none";
+
+    if(ultimoElementoFocado) {
+      ultimoElementoFocado.focus();
+    }
   }
 
   document.body.style.overflow = abrir ? "hidden" : "auto";
@@ -14,6 +54,7 @@ document.addEventListener("keydown", (event) => {
   if (event.key === "Escape") {
     alternarModal("ver-modal-inscrito", false);
     alternarModal("ver-modal-contato", false);
+    alternarModal("ver-modal-enviado", false);
 
     document.querySelectorAll(".cabecalho_lista-item").forEach((item) => {
       alternarSubmenu(item, false);
